45569314c5cd8c5e5e83d9ad1a24656d9f0fab8a
Yuchao Ding <ding-js@outlook.com>
2017-03-01 23:03:12 +0800

    始终完成度70%

--- src/pages/clock/clock.ts
+++ src/pages/clock/clock.ts
@@ -1,5 +1,6 @@
 interface IClockOptions {
 	radius?: number;
+	color?: string;
 }
 
 export default class Clock {
@@ -16,7 +17,8 @@ export default class Clock {
 		this._width = +canvas.getAttribute('width') || 300;
 		this._height = +canvas.getAttribute('height') || 300;
 		const _option: IClockOptions = {
-			radius: Math.min(this._width, this._height) / 2
+			radius: Math.min(this._width, this._height) / 2,
+			color: '#9b9b9b'
 		};
 
 		if (options) {
@@ -44,12 +46,14 @@ export default class Clock {
 				y: height / 2
 			},
 			op = this._options,
-			borderWidth = 10,
-			radius = op.radius;
+			color = op.color,
+			radius = op.radius,
+			borderWidth = radius * 0.08;
 		ctx.save();
 		ctx.clearRect(0, 0, width, height);
 
-		ctx.strokeStyle = '#a39999';
+		// 绘制边框
+		ctx.strokeStyle = color;
 		ctx.lineWidth = borderWidth;
 		ctx.beginPath();
 		ctx.arc(center.x, center.y, radius - borderWidth / 2, 0, 2 * Math.PI);
@@ -59,40 +63,87 @@ export default class Clock {
 
 		ctx.save();
 
+		// 绘制刻度
+		const distance2Top = borderWidth + radius * 0.02;
+
+		ctx.fillStyle = color;
+
+		ctx.translate(radius, radius);
+
+		ctx.rotate(-Math.PI / 30);
+
 		for (let i = 0; i < 60; i++) {
-			let scaleWidth = radius / 70;
-			const scaleRadius = radius - borderWidth - 5,
-				height = radius / 10,
-				r = i / 60 * 2 * Math.PI;
-			if (i % 5 === 0) {
-				scaleWidth = radius / 30;
-			}
+			const scaleWidth = i % 5 === 0 ? radius / 35 : radius / 70;
+			const scaleHeight = radius / 10;
+
+			const x = -scaleWidth / 2,
+				y = distance2Top - radius;
 
 
-
-
-
+			ctx.rotate(Math.PI / 30);
+			ctx.beginPath();
+			ctx.rect(x, y, scaleWidth, scaleHeight);
+			ctx.fill();
 		}
 
-		// window.requestAnimationFrame(this.draw);
+		ctx.setTransform(1, 0, 0, 1, 0, 0);
+
+		ctx.restore();
+
+		ctx.save();
+
+		// 绘制数字
+		const numberRadius = radius - (borderWidth + distance2Top + radius * 0.13);
+
+		const font = radius * 0.18;
+		ctx.fillStyle = color;
+		ctx.font = `${font}px sans-serif`;
+		ctx.textAlign = 'center';
+		ctx.textBaseline = 'middle';
+
+		for (let i = 1; i <= 12; i++) {
+			const radian = i / 12 * 2 * Math.PI,
+				x = radius + Math.sin(radian) * numberRadius,
+				y = radius - Math.cos(radian) * numberRadius;
+			ctx.fillText('' + i, x, y);
+		}
+
+		ctx.restore();
+
+		ctx.save();
+
+		const time = this.getCurrentTime();
+		// 绘制秒针
+		const secondRadius = radius * 0.75;
+
+		ctx.fillStyle = color;
+
+		ctx.beginPath();
+
+		ctx.moveTo(radius, radius);
+		ctx.lineTo(radius + secondRadius * Math.sin(time.sR), radius - secondRadius * Math.cos(time.sR));
+
+		ctx.stroke();
+
+		window.requestAnimationFrame(this.draw);
 	}
 
 
 
 	private getCurrentTime() {
 		const date = new Date();
-		const [h, m, s] = [
+		const [h, m, s, ms] = [
 			date.getHours(),
 			date.getMinutes(),
-			date.getSeconds()
+			date.getSeconds(),
+			date.getMilliseconds()
 		];
 
-		const [hR, mR, sR] = [h / 12, m / 60, s / 60].map(v => v * 2 * Math.PI);
+		const [hR, mR, sR] = [(h + m / 60) / 12, (m + s / 60) / 60, (s + ms / 1000) / 60].map(v => v * 2 * Math.PI);
 
 		return {
 			h, m, s, hR, mR, sR
 		};
 	}
-
 
 }
