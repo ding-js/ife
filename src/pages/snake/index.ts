aa36f7cae70f9233d677d4ef8c219e66d2c34722
Yuchao Ding <ding-js@outlook.com>
2017-03-09 20:54:06 +0800

    初始化贪食蛇

---
+++ src/pages/snake/index.ts
@@ -0,0 +1,3 @@
+import { Snake } from './snake';
+
+new Snake(document.querySelector('#snake-wrapper') as HTMLElement);
---
+++ src/pages/snake/snake.ts
@@ -0,0 +1,8 @@
+interface ISnakeOptions {
+
+}
+export class Snake {
+	constructor(container: HTMLElement, options?: ISnakeOptions) {
+
+	}
+}
