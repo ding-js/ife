652fe29e74ea20ec4f81f59a602579fb9660a22a
Yuchao Ding <ding-js@outlook.com>
2017-03-10 15:53:40 +0800

    贪食蛇主体逻辑完成

--- src/pages/snake/snake.ts
+++ src/pages/snake/snake.ts
@@ -1,4 +1,4 @@
-enum BoxStatus {
+enum BoxType {
 	empty,
 	wall,
 	body,
@@ -6,6 +6,21 @@ enum BoxStatus {
 	footer,
 	food
 }
+
+enum Direction {
+	left,
+	right,
+	up,
+	down
+}
+
+interface IDirection {
+	x: number;
+	y: number;
+	keyCode: number[];
+	opposite: number;
+}
+
 interface ISnakeOptions {
 	width?: number;
 	height?: number;
@@ -26,13 +41,14 @@ interface Ibox {
 	y: number;
 	xIndex: number;
 	yIndex: number;
-	type: BoxStatus;
+	type: BoxType;
 }
 
 interface IBoxType {
 	background?: string;
 	render?(ctx: CanvasRenderingContext2D, x: number, y: number, sideLength: number): void;
 }
+
 export class Snake {
 	private _container: HTMLElement;
 	private _options: ISnakeOptions;
@@ -40,10 +56,59 @@ export class Snake {
 	private _ctx: CanvasRenderingContext2D;
 	private _content: IContent;
 	private _boxes: Ibox[][] = [];
+	private _snake: Ibox[];
+	private _snakeDirection: Direction = Direction.right;
+	private _keys: Direction[] = [];
+
 	private _boxTypes: {
 		[key: number]: IBoxType;
+	} = {
+		[BoxType.empty]: {
+			background: '#f5f5f5'
+		},
+		[BoxType.wall]: {},
+		[BoxType.head]: {
+			background: 'red'
+		},
+		[BoxType.body]: {
+			background: 'green'
+		},
+		[BoxType.footer]: {
+			background: 'green'
+		},
+		[BoxType.food]: {
+			background: 'yellow'
+		}
 	};
-	private _snake: Ibox[];
+
+	private _direction: {
+		[key: number]: IDirection;
+	} = {
+		[Direction.left]: {
+			x: -1,
+			y: 0,
+			keyCode: [65, 37],
+			opposite: Direction.right
+		},
+		[Direction.right]: {
+			x: 1,
+			y: 0,
+			keyCode: [68, 39],
+			opposite: Direction.left
+		},
+		[Direction.up]: {
+			x: 0,
+			y: -1,
+			keyCode: [87, 38],
+			opposite: Direction.down
+		},
+		[Direction.down]: {
+			x: 0,
+			y: 1,
+			keyCode: [83, 40],
+			opposite: Direction.up
+		}
+	};
 
 	constructor(container: HTMLElement, options?: ISnakeOptions) {
 		const _options: ISnakeOptions = {
@@ -69,22 +134,12 @@ export class Snake {
 			colums: height / side
 		};
 
-		this._boxTypes = {
-			[BoxStatus.empty]: {
-				background: '#ccc'
-			},
-			[BoxStatus.wall]: {},
-			[BoxStatus.head]: {},
-			[BoxStatus.body]: {},
-			[BoxStatus.footer]: {},
-			[BoxStatus.food]: {}
-		};
-
 		this._container = container;
 		this._options = _options;
 
 		this.init();
 	}
+
 
 	private init() {
 		const op = this._options,
@@ -111,27 +166,122 @@ export class Snake {
 					y: content.y + y * side,
 					xIndex: x,
 					yIndex: y,
-					type: BoxStatus.empty
+					type: BoxType.empty
 				});
 			}
-
 			this._boxes.push(colum);
 		}
 
-		this._snake = [this._boxes[3][4], this._boxes[4][4], this._boxes[5][4]];
+		document.addEventListener('keydown', (e) => {
+			const direction = this._direction,
+				code = e.keyCode;
+			for (let i in direction) {
+				if (direction[i].keyCode.indexOf(code) > -1) {
+					e.preventDefault();
+					this._keys.push(+i);
+					// if (type !== snakeDirection && type !== direction[snakeDirection].opposite) {
+					// 	this._snakeDirection = type;
+					// }
+				}
+			}
+		});
 
-		this.update();
+		this.start();
 	}
 
-	private update() {
+	private getBox(x: number, y: number): Ibox {
+		const boxes = this._boxes;
+		const colum = boxes[x];
+
+		if (!colum) {
+			return;
+		}
+
+		return colum[y];
 
 	}
 
+	private start() {
+		const originSnake = [6, 5, 4, 3].map(x => this.getBox(x, 4));
+		this._snake = originSnake;
+		this.createAFood();
+		setInterval(this.update, 300);
+	}
+
+	private update = () => {
+		const keys = this._keys,
+			keysLen = keys.length;
+
+		console.log(this._keys);
+		if (keysLen > 0) {
+			for (let i = keysLen - 1; i >= 0; i--) {
+				const type = keys[i];
+				if (type !== this._snakeDirection && type !== this._direction[this._snakeDirection].opposite) {
+					this._snakeDirection = type;
+				}
+			}
+			this._keys = [];
+		}
+
+		const snake = this._snake,
+			direction = this._direction[this._snakeDirection],
+			content = this._content;
+
+		const head = snake[0],
+			footer = snake[snake.length - 1],
+			x = head.xIndex + direction.x,
+			y = head.yIndex + direction.y;
+
+		const box = this.getBox(x, y) as Ibox;
+
+		if (box && x >= 0 && x < content.colums && y >= 0 && y < content.rows) {
+			switch (box.type) {
+				case BoxType.empty:
+					footer.type = BoxType.empty;
+					snake.pop();
+					break;
+				case BoxType.food:
+					this.createAFood();
+					break;
+				default:
+					return;
+			}
+
+			snake.unshift(box);
+			snake.forEach(s => {
+				s.type = BoxType.body;
+			});
+			snake[0].type = BoxType.head;
+			snake[snake.length - 1].type = BoxType.footer;
+
+			this.draw();
+
+		} else {
+			console.info('Game over!', ' : ', '撞墙');
+		}
+
+	}
+
+	private createAFood() {
+		const content = this._content,
+			x = Math.floor(Math.random() * content.colums),
+			y = Math.floor(Math.random() * content.rows),
+			box = this.getBox(x, y) as Ibox;
+
+		if (box.type === BoxType.empty) {
+			box.type = BoxType.food;
+		} else {
+			return this.createAFood();
+		}
+
+		return box;
+	}
 	private draw() {
 		const op = this._options,
 			ctx = this._ctx,
 			types = this._boxTypes,
 			side = op.sideLength;
+
 		ctx.clearRect(0, 0, op.width, op.height);
 
 		ctx.save();
