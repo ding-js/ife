a1e87b7d624c0a668d8b46dd70b93541d38519d8
Yuchao Ding <ding-js@outlook.com>
2017-11-08 22:22:59 +0800

    init vue

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+
+Vue.config.productionTip = false;
+
+// tslint:disable-next-line:no-unused-expression
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+});
---
+++ typings/index.d.ts
@@ -0,0 +1,6 @@
+declare module '*.vue' {
+  import Vue from 'vue';
+  const component;
+  export default component;
+  // export default typeof Vue;
+}
