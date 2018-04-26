03c70157760b71a972967f0b6e5458acd85eed80
Yuchao Ding <ding-js@outlook.com>
2018-04-26 21:48:08 +0800

    webpack: add public path env

--- config/index.js
+++ config/index.js
@@ -10,8 +10,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: './',
-    assetsPublicPath: '/ife/dist/',
-    // assetsPublicPath: '/dist/',
+    assetsPublicPath: process.env.PUBLIC_PATH || '/ife/dist/',
     productionSourceMap: false,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
