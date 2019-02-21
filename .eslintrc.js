3d134a3d942ca99429dc8c2264220d497af38349
Yuchao Ding <ding-js@outlook.com>
2019-02-21 11:29:54 +0800

    fix: eslint with --no-optional

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,32 +5,12 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
-  parserOptions: {
-    parser: 'typescript-eslint-parser'
-  },
+
   overrides: [
     {
-      files: [
-        '**/tests/unit/**/*.spec.{js,jsx,ts,tsx}',
-        '**/__tests__/*.{js,jsx,ts,tsx}'
-      ],
+      files: ['**/tests/unit/**/*.spec.{js,jsx,ts,tsx}', '**/__tests__/*.{js,jsx,ts,tsx}'],
       env: {
         jest: true
-      },
-      globals: {
-        page: true,
-        browser: true,
-        context: true,
-        jestPuppeteer: true
-      }
-    },
-    {
-      files: ['./*.js'],
-      parserOptions: {
-        parser: 'babel-parser'
-      },
-      env: {
-        node: true
       }
     }
   ]
